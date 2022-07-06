5bebf83c6c078fcaafb9b9fd5544e501d20bed2b
punxund <52384346+punxund@users.noreply.github.com>
2022-07-07 00:28:48 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -101,7 +101,7 @@ while True:
                 sys.exit(0)
             except : 
                 pass    
-            if randint(1, 5) == 1:
+            if randint(1, 20) == 1:
                 try :
                     server = Server()
                 except KeyboardInterrupt:
